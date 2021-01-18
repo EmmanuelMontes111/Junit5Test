3f6cfa7ea17512af9990dacd4dffcae1d47032a6
EmmanuelMontes111 <emmanuelmontes111@gmail.com>
2021-01-17 20:39:15 -0500

    WebService and Login created whit Test

--- src/main/java/mockito/WebService.java
+++ src/main/java/mockito/WebService.java
@@ -3,4 +3,19 @@ package mockito;
 public class WebService {
 
 
+    public void login(String user, String password, Callback callback){
+        if(checkLogin(user, password)){
+            callback.onSuccess("User Correct");
+        }
+        else{
+            callback.onFail("Error");
+        }
+    }
+
+    public boolean checkLogin(String user, String password){
+        if(user.equals("Alberto") && password.equals("1234")){
+            return true;
+        }
+    return false;
+    }
 }
---
+++ src/test/java/mockito/WebServiceTest.java
@@ -0,0 +1,52 @@
+package mockito;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+class WebServiceTest {
+
+    private WebService webService;
+
+    @Mock
+    private Callback callback;
+
+    @BeforeEach
+    public void setUp() {
+        webService = new WebService();
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void checkLoginTest(){
+        assertTrue(webService.checkLogin("Alberto", "1234"));
+    }
+
+    @Test
+    public void checkLoginErrorTest(){
+        assertFalse(webService.checkLogin("Maria", "0987"));
+    }
+
+
+    @Test
+    public void loginTest(){
+        webService.login("Alberto", "1234", callback);
+        verify(callback).onSuccess("User Correct");
+    }
+
+
+    @Test
+    public void loginErrorTest(){
+        webService.login("Emmanuel", "1234", callback);
+        verify(callback).onFail("Error");
+    }
+}
